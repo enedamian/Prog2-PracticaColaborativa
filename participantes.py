590529e5d1282af9993360c0c6c8e969e9eab99b
enedamian <142589183+enedamian@users.noreply.github.com>
2023-08-29 10:25:02 -0300

    Subo las primeras lineas con mi saludo

---
+++ participantes.py
@@ -0,0 +1,7 @@
+
+print("------------------------------------------------------------------")
+print("Bienvenidos a la práctica de github!")
+print("Este es un ejercicio colaborativo para practicar el uso de github.")
+print("------------------------------------------------------------------")
+print("Vamos a agregar nuestros nombres a continuación:")
+print("Acá estuvo Damian Ene.")
