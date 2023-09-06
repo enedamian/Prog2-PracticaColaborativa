341d73755c55c8d2ae8331892befb5ddcd3b63cf
enedamian <142589183+enedamian@users.noreply.github.com>
2023-09-06 09:37:16 -0300

    nuevo conflicto

--- participantes.py
+++ participantes.py
@@ -4,11 +4,8 @@ print("Bienvenidos a la práctica de github!")
 print("Este es un ejercicio colaborativo para practicar el uso de github.")
 print("------------------------------------------------------------------")
 print("Vamos a agregar nuestros nombres a continuación:")
-print()
 print("Acá estuvo Damian Ene. Se creó el PR y fue controlado por 'pepe'.")
 
-print("Matias Nicolas Cappa y lo controlo Lucas Bertone")
-print("Aca estuvo Lucas Bertone")
 print("En main ustedes ya aprobaron PRs con sus nombres, si hago merge con esto, ¿que pasara?")
 
 print("Generando conflictos!!! MUEJEJE")
