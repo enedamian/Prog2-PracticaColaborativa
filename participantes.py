2537e6d28dca8a595c6bcaff17b45959369bf7a5
enedamian <142589183+enedamian@users.noreply.github.com>
2023-09-06 08:26:16 -0300

    restauracion de lineas borradas

--- participantes.py
+++ participantes.py
@@ -7,6 +7,8 @@ print("Vamos a agregar nuestros nombres a continuación:")
 print()
 print("Acá estuvo Damian Ene. Se creó el PR y fue controlado por 'pepe'.")
 
+print("Matias Nicolas Cappa y lo controlo Lucas Bertone")
+print("Aca estuvo Lucas Bertone")
 print("En main ustedes ya aprobaron PRs con sus nombres, si hago merge con esto, ¿que pasara?")
 
 print("Generando conflictos!!! MUEJEJE")
