fc98dd01e22b1e5b5d6cfe1d4d1cca8b68903ac4
MatCapPa <cappa.nicolas1@gmail.com>
2023-09-06 22:16:45 -0300

    cambio

--- participantes.py
+++ participantes.py
@@ -7,13 +7,10 @@ print("Vamos a agregar nuestros nombres a continuación:")
 print("Matias Nicolas Cappa y lo controlo Lucas Bertone")
 #print("Acá estuvo Damian Ene. Se creó el PR y fue controlado por 'pepe'.")
 print()
-<<<<<<< HEAD
 print()
-=======
 print("Aca estuvo Lucas Bertone")
 print("Acá estuvo Damian Ene. Se creó el PR y fue controlado por 'pepe'.")
 print()
 print("Acá estuvo Damian Ene. Se creó el PR y fue controlado por 'pepe'.")
 print("Aca estuvo bautista menghini.")
 print("Acá estuvo Aldana Aranda.")
->>>>>>> 8eb14dbc41e2064dd670e10a52e37a70e4b2bdc0
